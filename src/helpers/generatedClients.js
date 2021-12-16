9b1e8d9a25bda0dd7a94b865c2c6daf40851a6b2
Charles Kasasira <charleskasasira01@gmail.com>
2021-12-16 16:27:31 +0300

    generated Clients data

---
+++ src/helpers/generatedClients.js
@@ -0,0 +1,89 @@
+const generatedClients = [
+    {
+        name: "David Anyuru One",
+        gender: "M",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Two",
+        gender: "M",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Three",
+        gender: "F",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Four",
+        gender: "M",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Five",
+        gender: "M",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Six",
+        gender: "M",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Seven",
+        gender: "M",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Eight",
+        gender: "M",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Nine",
+        gender: "F",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Ten",
+        gender: "F",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Eleven",
+        gender: "M",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    },
+    {
+        name: "David Anyuru Twelve",
+        gender: "F",
+        email: "davidderrick@gmail.com",
+        contact: "077123456",
+        address: "Busenga"
+    }
+]
+
+
+export default generatedClients
