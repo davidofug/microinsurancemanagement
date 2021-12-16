c018586cb6ec79c5defe63966cb4528285b1db35
Charles Kasasira <charleskasasira01@gmail.com>
2021-12-16 06:37:54 +0300

    index.js and index.html code refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+import './index.css'
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AuthProvider from './contexts/Auth';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
-
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { combineProviders } from './helpers/combineProviders';
-import AuthProvider from './contexts/Auth';
+
 
 const Providers = combineProviders([AuthProvider]);
 ReactDOM.render(
@@ -18,7 +18,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
