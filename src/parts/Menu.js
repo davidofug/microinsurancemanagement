e9f174eda6150977e0898fa73f5de21c8c9223cc
Charles Kasasira <charleskasasira01@gmail.com>
2021-12-16 10:17:03 +0300

    menu refactor

--- src/parts/Menu.js
+++ src/parts/Menu.js
@@ -19,82 +19,61 @@ function Menu() {
 
     return (
         <div>
-            {toggleMenu === true ?
+            {toggleMenu === true 
+            ?
                 <nav className='navbar'>
-                <div id='brand'>
-                    <div id="brand_logo">
-                        <img src={logo} alt="Britam" />
-                        <i onClick={() => {
-                            setToggeMenu(!toggleMenu)
-                        }}><CloseIcon /></i>
-                    </div>
-                    <hr />
-                </div>
-                
-                <div id="menu">
-                    <ul>
-                        {
-                            selected.menuData.map((object, index) => (
-                                    <Link to={`${object.link}`} className={toggleActiveStyle(index)} onClick={() => {
-                                        toggleActive(index)
-                                    }} key={index}>
-                                        <i className='icon'>{object.icon}</i>
-                                        {object.name}
-                                    </Link>
-                                )
-                            )
-                        }
-                    </ul>
-                    
-                </div>
-                
-                <div >
-                    <Link to="settings" id="account">
-                        <img src={profile} alt="image" />
-                        <p style={{"color": "black"}}>Charles Kasasira</p>
-                        <div id="eclipse">
-                            <div className='circle'></div>
-                            <div className='circle'></div>
-                            <div className='circle'></div>
+                    <div id='brand'>
+                        <div id="brand_logo">
+                            <img src={logo} alt="Britam" />
+                            <i onClick={() => setToggeMenu(!toggleMenu)}><CloseIcon /></i>
                         </div>
+                        <hr />
+                    </div>
+                
+                    <div id="menu">
+                            { selected.menuData.map((object, index) => (
+                                        <Link to={`${object.link}`} className={toggleActiveStyle(index)} onClick={() => toggleActive(index)} key={index}>
+                                            <i className='icon'>{object.icon}</i> {object.name}
+                                        </Link>
+                                    )
+                                )
+                            }
+                    </div>
+                
+                    <Link to="settings" id="account">
+                        <img src={profile} alt="profile image" />
+                        <p style={{"color": "black"}}>Charles Kasasira</p>
+                        <div id="eclipse"><div></div><div></div><div></div></div>
                     </Link>
-                </div>
 
                 </nav> 
             : 
-            <nav className='navbar navbar-m'>
-            <div id='brand-m'>
-                <div id="brand_logo">
-                    <i onClick={() => {
-                        setToggeMenu(!toggleMenu)
-                    }}><ViewHeadlineIcon/></i>
-                </div>
-                <hr />
-            </div>
-            
-            <div id="menu">
-                <ul>
-                    {
-                        selected.objects.map((object, index) => (
-                                <Link to={`${object.link}`} className={toggleActiveStyle(index)} onClick={() => {
-                                    toggleActive(index)
-                                }} key={index}>
-                                    <i>{object.icon}</i>
-                                </Link>
-                            )
-                        )
-                    }
-                </ul>
+                <nav className='navbar navbar-m'>
+                    <div id='brand-m'>
+                        <div id="brand_logo">
+                            <i onClick={() => setToggeMenu(!toggleMenu)}><ViewHeadlineIcon/></i>
+                        </div>
+                        <hr />
+                    </div>
                 
-            </div>
-            
-            <div id="account">
-                <Link to="settings" id="account">
-                    <img src={profile} alt="image" />
-                </Link>
-            </div>
+                    <div id="menu">
+                            {
+                                selected.menuData.map((object, index) => (
+                                        <Link to={`${object.link}`} className={toggleActiveStyle(index)} onClick={() =>toggleActive(index)} key={index}>
+                                            <i>{object.icon}</i>
+                                        </Link>
+                                    )
+                                )
+                            }
+                    </div>
+                
+                    <div id="account">
+                        <Link to="settings" id="account">
+                            <img src={profile} alt="image" />
+                        </Link>
+                    </div>
 
-            </nav> 
+                </nav> 
             }
         </div>
     )
