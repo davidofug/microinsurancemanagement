ad271f1073a6fba76f2f7814614ea03324bda11f
Charles Kasasira <charleskasasira01@gmail.com>
2021-12-20 11:45:59 +0300

    removed link on forgotPassword

--- src/pages/admin/Organisations.js
+++ src/pages/admin/Organisations.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 function Organisations() {
 
@@ -13,101 +14,111 @@ function Organisations() {
                 <p className="subtitle">Manage Organsations</p>
             </div>
 
-            <div class="componentsData table">
+            <div id="add_client_group">
+                <div></div>
+                <Link to="/add-clients">
+                    <button className="btn btn-primary cta">Add Organisations</button>
+                </Link>
+                
+            </div>
+
+            <div class="componentsData">
                 <div className="table-card">
                     <div id="search">
                         <input type="text" placeholder='Search for client...' id='searchInput' />
                         <button className='btn btn-primary'>Search</button>
                         <button className='btn btn-primary'>Export</button>
                     </div>
-                    <table class="table table-striped" style={{border: "1px solid black"}}>
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Gender</th>
-                            <th>Email</th>
-                            <th>Contact</th>
-                            <th>Address</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td></td>
-                            <td>David Anyuru</td>
-                            <td>M</td>
-                            <td>davidderrick@gmail.com</td>
-                            <td>077123456</td>
-                            <td>Busenga</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td>David Anyuru</td>
-                            <td>M</td>
-                            <td>davidderrick@gmail.com</td>
-                            <td>077123456</td>
-                            <td>Busenga</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td>David Anyuru</td>
-                            <td>M</td>
-                            <td>davidderrick@gmail.com</td>
-                            <td>077123456</td>
-                            <td>Busenga</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td>David Anyuru</td>
-                            <td>M</td>
-                            <td>davidderrick@gmail.com</td>
-                            <td>077123456</td>
-                            <td>Busenga</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td>David Anyuru</td>
-                            <td>M</td>
-                            <td>davidderrick@gmail.com</td>
-                            <td>077123456</td>
-                            <td>Busenga</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td>David Anyuru</td>
-                            <td>M</td>
-                            <td>davidderrick@gmail.com</td>
-                            <td>077123456</td>
-                            <td>Busenga</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td>David Anyuru</td>
-                            <td>M</td>
-                            <td>davidderrick@gmail.com</td>
-                            <td>077123456</td>
-                            <td>Busenga</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td>David Anyuru</td>
-                            <td>M</td>
-                            <td>davidderrick@gmail.com</td>
-                            <td>077123456</td>
-                            <td>Busenga</td>
-                        </tr>
-                        </tbody>
-                        <tfoot>
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Gender</th>
-                            <th>Email</th>
-                            <th>Contact</th>
-                            <th>Address</th>
-                        </tr>
-                        </tfoot>
-                    </table>
+                    <div className="table-responsive">
+                        <table class="table table-striped" style={{border: "1px solid black"}}>
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Gender</th>
+                                <th>Email</th>
+                                <th>Contact</th>
+                                <th>Address</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td></td>
+                                <td>David Anyuru</td>
+                                <td>M</td>
+                                <td>davidderrick@gmail.com</td>
+                                <td>077123456</td>
+                                <td>Busenga</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>David Anyuru</td>
+                                <td>M</td>
+                                <td>davidderrick@gmail.com</td>
+                                <td>077123456</td>
+                                <td>Busenga</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>David Anyuru</td>
+                                <td>M</td>
+                                <td>davidderrick@gmail.com</td>
+                                <td>077123456</td>
+                                <td>Busenga</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>David Anyuru</td>
+                                <td>M</td>
+                                <td>davidderrick@gmail.com</td>
+                                <td>077123456</td>
+                                <td>Busenga</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>David Anyuru</td>
+                                <td>M</td>
+                                <td>davidderrick@gmail.com</td>
+                                <td>077123456</td>
+                                <td>Busenga</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>David Anyuru</td>
+                                <td>M</td>
+                                <td>davidderrick@gmail.com</td>
+                                <td>077123456</td>
+                                <td>Busenga</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>David Anyuru</td>
+                                <td>M</td>
+                                <td>davidderrick@gmail.com</td>
+                                <td>077123456</td>
+                                <td>Busenga</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>David Anyuru</td>
+                                <td>M</td>
+                                <td>davidderrick@gmail.com</td>
+                                <td>077123456</td>
+                                <td>Busenga</td>
+                            </tr>
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Gender</th>
+                                <th>Email</th>
+                                <th>Contact</th>
+                                <th>Address</th>
+                            </tr>
+                            </tfoot>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
