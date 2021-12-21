0baaae74ceb6f5eb4b0260272ef8a55c3d53a53a
Charles Kasasira <charleskasasira01@gmail.com>
2021-12-21 15:48:15 +0300

    Searchable Data tables

--- src/pages/Comprehensive.js
+++ src/pages/Comprehensive.js
@@ -2,12 +2,15 @@ import { useEffect } from 'react'
 import generatedData from '../helpers/generatedClients';
 import { Link } from 'react-router-dom'
 import { Table } from 'react-bootstrap'
+import { MDBDataTable } from 'mdbreact'
+import stickerData from '../helpers/Sticker-fake-data';
 
 function Comprehensive() {
 
     useEffect(() => {
         document.title = 'Britam - Comprehensive'
     }, [])
+
 
     return (
         <div className='components'>
@@ -20,39 +23,15 @@ function Comprehensive() {
                 <Link to="admin-policies"><button className="btn btn-primary cta">Add</button></Link>
             </div>
 
-            <div className="table-card">   
-                <div id="search">
-                <input type="text" placeholder='filter by Dates..' id='searchInput' />
-                    <input type="text" placeholder='Search' id='searchInput' />
+            <div class="componentsData">
+                <div className="table-card">
+                    <MDBDataTable
+                        striped
+                        bordered
+                        responsive
+                        data={stickerData}
+                    />
                 </div>
-
-                <Table striped responsive bordered hover style={{border: "1px solid black"}}>
-                    <thead>
-                        <tr><th>Client Name</th><th>Category</th><th>Amount</th><th>Payment Method</th><th>Currency</th><th>Agent</th><th>Status</th><th>CreatedAt</th><th></th></tr>
-                    </thead>
-
-                    <tbody>
-                        
-                            
-                                {generatedData[0].map((generatedClient, index) => (
-                                    <tr>
-                                        <td>{generatedClient.refNumber}</td>
-                                        <td>{generatedClient.email}</td>
-                                        <td>{generatedClient.date}</td>
-                                        <td>{generatedClient.plateNumber}</td>
-                                        <td>{generatedClient.stickerNumber}</td>
-                                        <td>{generatedClient.estimate}</td>
-                                        <td>{generatedClient.status}</td>
-                                        <td>13/09/2000</td>
-                                        <td><div id="action"><div></div><div></div><div></div></div></td>
-                                    </tr>
-                                ))}
-                    </tbody>
-
-                    <tfoot>
-                    <tr><th>Notification Ref No.</th><th>Claimant Details</th><th>Date of Incident</th><th>Number Plate</th><th>Sticker No.</th><th>Claim Estimate</th><th>Status</th>CreatedAt<th><th></th></th></tr>
-                    </tfoot>
-                </table>
             </div>
         </div>
     )
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,4 +1,7 @@
 import {useEffect} from 'react'
+import { MDBDataTable } from 'mdbreact'
+import stickerData from '../helpers/Sticker-fake-data'
+import { MdDownload } from 'react-icons/md'
 
 function Users() {
 
@@ -14,99 +17,21 @@ function Users() {
 
             <div className="table-card componentsData">   
                 <div id="search">
-                    <input type="text" placeholder='Search for client...' id='searchInput' />
-                    <button className='btn btn-primary'>Search</button>
-                    <button className='btn btn-primary'>Export</button>
+                    <div></div>
+                    <div></div>
+                    <button className='btn btn-primary'>Export <MdDownload /></button>
                 </div>
 
-                <table class="table table-striped" style={{border: "1px solid black"}}>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Gender</th>
-                        <th>Email</th>
-                        <th>Contact</th>
-                        <th>Address</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td></td>
-                        <td>David Anyuru</td>
-                        <td>M</td>
-                        <td>davidderrick@gmail.com</td>
-                        <td>077123456</td>
-                        <td>Busenga</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>David Anyuru</td>
-                        <td>M</td>
-                        <td>davidderrick@gmail.com</td>
-                        <td>077123456</td>
-                        <td>Busenga</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>David Anyuru</td>
-                        <td>M</td>
-                        <td>davidderrick@gmail.com</td>
-                        <td>077123456</td>
-                        <td>Busenga</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>David Anyuru</td>
-                        <td>M</td>
-                        <td>davidderrick@gmail.com</td>
-                        <td>077123456</td>
-                        <td>Busenga</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>David Anyuru</td>
-                        <td>M</td>
-                        <td>davidderrick@gmail.com</td>
-                        <td>077123456</td>
-                        <td>Busenga</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>David Anyuru</td>
-                        <td>M</td>
-                        <td>davidderrick@gmail.com</td>
-                        <td>077123456</td>
-                        <td>Busenga</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>David Anyuru</td>
-                        <td>M</td>
-                        <td>davidderrick@gmail.com</td>
-                        <td>077123456</td>
-                        <td>Busenga</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>David Anyuru</td>
-                        <td>M</td>
-                        <td>davidderrick@gmail.com</td>
-                        <td>077123456</td>
-                        <td>Busenga</td>
-                    </tr>
-                    </tbody>
-                    <tfoot>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Gender</th>
-                        <th>Email</th>
-                        <th>Contact</th>
-                        <th>Address</th>
-                    </tr>
-                    </tfoot>
-                </table>
+                <div class="componentsData">
+                <div className="table-card">
+                    <MDBDataTable
+                            striped
+                            bordered
+                            responsive
+                            data={stickerData}
+                        />
+                </div>
+            </div>
             </div>
         </div>
         
