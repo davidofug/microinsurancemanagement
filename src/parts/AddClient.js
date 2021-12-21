0d30b803f47085770f7f766bf1214c0c18f6cb3e
anyuru <davidderrickanyuru@gmail.com>
2021-12-21 18:18:29 +0300

    Clearing some clutter

--- src/parts/AddClient.js
+++ src/parts/AddClient.js
@@ -1,30 +1,8 @@
-import { useState }from 'react'
-// import Modal from 'react-bootstrap'
-import { Modal, Button } from 'react-bootstrap'
-
 function AddClient(){ 
     
     return (
         <>
-        {/* <Button variant="primary" onClick={handleShow}>
-            Launch demo modal
-        </Button>
-
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-            <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-                Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-                Save Changes
-            </Button>
-            </Modal.Footer>
-        </Modal> */}
-        haha it worked 
+            haha it worked 
         </>
 );
 }
