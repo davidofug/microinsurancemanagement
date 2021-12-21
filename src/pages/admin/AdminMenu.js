50c0f6d9f4ac63d4d85d7ca29f4be4d0089f758c
Charles Kasasira <charleskasasira01@gmail.com>
2021-12-21 06:56:01 +0300

    RWD: added a mobile navbar

--- src/pages/admin/AdminMenu.js
+++ src/pages/admin/AdminMenu.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react'
 import profile from '../../assets/imgs/image 2.png'
 import logo from '../../assets/imgs/britam-logo.png'
 import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi'
+import { Navbar, Offcanvas, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
 
 
 function AdminMenu({setLargeContentClass, largeContentClass}) {
@@ -38,48 +39,74 @@ function AdminMenu({setLargeContentClass, largeContentClass}) {
 
     return (
         <div className='menuSide'>
-            <nav className='mobile-sidebar'>
-                        <section id='brand_m'>
-                                {/* <i onClick={() => setToggeMenu(!toggleMenu)}>
-                                <HiOutlineChevronRight />
-                                    </i> */}
-                        </section>
+                <div class="mobile-sidebar">
+                    <Navbar expand={false} >
+                        <Container fluid>
+                            <div>
+                                <Navbar.Toggle className='m-3' aria-controls="offcanvasNavbar" />
+                                <Navbar.Brand href="#">
+                                    <img src={logo} alt="Britam" />
+                                </Navbar.Brand>
+                            </div>
+                            <Navbar.Offcanvas
+                            id="offcanvasNavbar"
+                            aria-labelledby="offcanvasNavbarLabel"
+                            placement="start"
+                            >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id="offcanvasNavbarLabel">
                     
-                        <section className='position-sticky pt-3' id="menu_section">
-                                    <ul className="nav flex-column">
-                                        { selected.Admin.map((object, index) => (
-                                                            <li className='nav-item' key={index}>
-                                                                <Link to={object.link} className={toggleActiveClassStyle(index)} onClick={() => toggleActive(index)} key={index} >
-                                                                    <span>{object.icon}</span>
-                                                                    {object?.subMenu &&
-                                                                        (<ul>
-                                                                            {object.subMenu.map((sub, index) => (
-                                                                                <li>
-                                                                                    <Link to={sub.link} key={index} style={{color: "black"}}>
-                                                                                        {sub.name}
-                                                                                    </Link>
-                                                                                </li>
-                                                                            ))}
-                                                                        </ul>)
-                                                                    }
-                                                                </Link>
-                                                            </li>
+                                        <img src={logo} alt="Britam" />
+                    
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <ul className="nav flex-column">
+                                            { selected.Admin.map((object, index) => (
+                                                                <li className='nav-item' key={index}>
+                                                                    <Link to={object.link} className={toggleActiveClassStyle(index)} onClick={() => toggleActive(index)} key={index} >
+                                                                        <span>{object.icon}</span>{object.name}
+                                                                        {object?.subMenu &&
+                                                                            (
+                                                                                <NavDropdown style={{"color": "#fff"}} id="offcanvasNavbarDropdown">
+                                                                                {object.subMenu.map((sub, index) => (
+                                                                                            <Link to={sub.link}>
+                                                                                                <NavDropdown.Item href="#action3">{sub.name}</NavDropdown.Item>
+                                                                                            </Link>
+                    
+                                                                                    ))}
+                                                                                </NavDropdown>
+                                                                            )
+                                                                        }
+                                                                    </Link>
+                                                                </li>
+                                                    )
                                                 )
-                                            )
-                                        }
-                                    </ul>
-                            </section>
+                                            }
+                                        </ul>
                     
-                        <footer>
-                                <ul>
-                                    <li><Link to="/admin-settings">Settings</Link></li>
-                                    <li><Link to="/logout">Logout</Link></li>
-                                </ul>
-                            <Link to={'/admin-settings'} id="account">
-                                <img src={profile} alt="profile image" />
-                            </Link>
-                        </footer>
-                    </nav>
+                                </Nav>
+                            </Offcanvas.Body>
+                            <footer>
+                                        <ul>
+                                            <li><Link to="/admin-settings">My Profile</Link></li>
+                                            <li><Link to="/logout">Logout</Link></li>
+                                        </ul>
+                                    <Link to={'/admin-settings'}>
+                                        <img src={profile} alt="profile image" />
+                                        <div>
+                                            <p>Charles Kasasira</p>
+                                            <p style={{"color": "#646464"}}>Admin</p>
+                                        </div>
+                                        <div id="eclipse"><div></div><div></div><div></div></div>
+                                    </Link>
+                                </footer>
+                            </Navbar.Offcanvas>
+                        </Container>
+                        </Navbar>
+                </div>
+
             {toggleMenu === true 
             ?
                 <>
