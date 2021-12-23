5dc8d84de4df7227bc34ab6fd549b6e092921d02
anyuru <davidderrickanyuru@gmail.com>
2021-12-23 14:11:38 +0300

    Dashboard mobile phone responsiveness

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -27,42 +27,42 @@ function Dashboard() {
                 </header>
 
                 <div className="componentsData" >
-                    <div className='mb-5' style={{display:"flex", gap:"2.5vw", width: "100%", justifyContent: "space-between"}}>
+                    <div className='mb-5 first-row' style={{display:"flex", gap:"6vh", width: "100%", justifyContent: "space-between"}}>
                         <div className="shadow-sm p-3 bg-body rounded" className="first-container">
                             <Container className="row-container">
                                     <div className="col">
-                                        <Card className="card" style={{"background-color":"#804C75"}}>
+                                        <div className="custom-card" style={{"background-color":"#804C75"}}>
                                             <Card.Body className="card-body">
                                                 <div className="statistics">{`${claims}`}</div>
                                                 <div className="card-text">Claim Settlement</div>
                                             </Card.Body>
-                                        </Card>
+                                        </div>
                                     </div>
                                     <div className="col">
-                                        <Card className="card" style={{"background-color":"#FFB848"}}>
+                                        <div className="custom-card" style={{"background-color":"#FFB848"}}>
                                             <Card.Body className="card-body">
                                                 <div className="statistics">{`${policies}`}</div>
                                                 <div className="card-text">Policies</div>
                                             </Card.Body>
-                                        </Card>
+                                        </div>
                                     </div>
                             </Container>
                             <Container className="row-container">
                                     <div className="col">
-                                        <Card className="card" style={{"background-color":"#C82E29"}}>
+                                        <div className="custom-card" style={{"background-color":"#C82E29"}}>
                                             <Card.Body className="card-body">
                                                 <div className="statistics">{`${stickers}`}</div>
                                                 <div className="card-text">Stickers</div>
                                             </Card.Body>
-                                        </Card>
+                                        </div>
                                     </div>
                                     <div className="col">
-                                        <Card className="card" style={{"background-color":"#1FBBA6"}}>
+                                        <div className="custom-card" style={{"background-color":"#1FBBA6"}}>
                                             <Card.Body className="card-body">
                                                 <div className="statistics">{`${claimNotifications}`}</div>
                                                 <div className="card-text">Claim Notifications</div>
                                             </Card.Body>
-                                        </Card>
+                                        </div>
                                     </div>
                             </Container>
                         </div>
@@ -70,7 +70,7 @@ function Dashboard() {
                         <div className="shadow-sm p-3 bg-body rounded summaries " className="first-container" >
                             {/* Are these supposed to be links or just mere words? */}
                             <div id="short_stats">
-                                <h3>Agent issued reports</h3>
+                                <h3 className="heading">Agent issued reports</h3>
                                 <Table responsive borderless>
                                     <thead><th></th><th>Grand totals</th></thead>
                                     <tbody>
@@ -95,21 +95,21 @@ function Dashboard() {
                         <h5 style={{"display":"flex", "gap": "10px"}}><span>
                             <div style={{"width": "20px", "height": "20px", "background-color": "#E0E7EC"}}></div>
                         </span>Monthly Stickers Issued</h5>
-                        <Row style={{paddingTop:"3vh", paddingBottom:"2vh", paddingRight:"3vh"}}>
-                            <Col xs="1" style={{display:"flex", justifyContent:"center", writingMode:"tb-rl", transform:"rotate(-180deg)", alignItems:"End"}}>
+                        <Row id="graph" style={{paddingTop:"3vh", paddingBottom:"2vh", paddingRight:"3vh"}}>
+                            {/* <Col xs="1" id="vertical-scale-name" style={{display:"flex", justifyContent:"center", writingMode:"tb-rl", transform:"rotate(-180deg)", alignItems:"End"}}>
                                 <span>Sticker sales</span>    
-                            </Col>
-                            <Col>
-                                <BarChart />
+                            </Col> */}
+                            <Col style={{height:"35vh"}}>
+                                    <BarChart />
                             </Col>
                         </Row>
                         <Row style={{diplay:"flex", justifyContent:"center"}}>
                             <Col>Months</Col>
                         </Row>
                     </div>
-                </div>    
-            </div>        
-    )
+                </div>  
+            </div>
+        )
 }
 
 export default Dashboard
