a2b0b32f38b5d22c7a5cbace0dc71fa7bb3e57e1
David Wampamba <davidwampamba@gmail.com>
2021-12-23 12:04:22 +0300

    Decouple Auth context and provider

--- src/contexts/Auth.js
+++ src/contexts/Auth.js
@@ -7,34 +7,6 @@ import {
 
 const AuthContext = createContext()
 
-export function useAuth() {
+export default function useAuth() {
     return useContext( AuthContext )
 }
-
-function AuthProvider({ children }) {
-
-    const [currentUser, setCurrentUser] = useState(null)
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        const loggedIn = parseInt(localStorage.getItem('loggedIn'))
-        if(loggedIn === 1 || loggedIn === 2 || loggedIn === 3 || loggedIn === 4) {
-            setCurrentUser(loggedIn)
-        }
-    },[])
-
-    const value = {
-        loading,
-        currentUser,
-        setCurrentUser,
-        setLoading
-    }
-
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
-
-export default AuthProvider
