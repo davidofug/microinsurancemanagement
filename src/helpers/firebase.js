f7258393ad4d04703f175ea6d9905c493da7823b
David Wampamba <davidwampamba@gmail.com>
2021-12-23 11:34:23 +0300

    Firebase integration

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -2,13 +2,13 @@ import {initializeApp} from 'firebase/app';
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCMyQY2AE1qal8Kzb9DTOMJNZ2a8OjNaZc",
-    authDomain: "britam-apps.firebaseapp.com",
-    projectId: "britam-apps",
-    storageBucket: "britam-apps.appspot.com",
-    messagingSenderId: "679626174983",
-    appId: "1:679626174983:web:6a34512b4e8ca737b5f0a2",
-    measurementId: "G-V2PDNNTC1W"
+    apiKey: REACT_APP_apiKey,
+    authDomain: REACT_APP_authDomain,
+    projectId: REACT_APP_projectId,
+    storageBucket: REACT_APP_storageBucket,
+    messagingSenderId: REACT_APP_messagingSenderId,
+    appId: REACT_APP_appId,
+    measurementId: REACT_APP_measurementId
 }
 
 const app = initializeApp(firebaseConfig)
