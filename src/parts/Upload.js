2a193f53e9acb189cab1cc9bf23d0d784520534e
Charles Kasasira <charleskasasira01@gmail.com>
2021-12-24 04:22:52 +0300

    upload component

--- src/parts/Upload.js
+++ src/parts/Upload.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Form} from 'react-bootstrap'
 
 export default function Upload() {
     return (
-        <div>
-            
+        <div className='upload-container'>
+            {/* <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label>upload profile image</Form.Label>
+                <Form.Control type="file" />
+            </Form.Group> */}
+            <input type="file" className='file-upload' />
         </div>
     )
 }
