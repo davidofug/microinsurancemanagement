ac5abff2cdb0aecc82554ff7363295628ca71ca2
Charles Kasasira <charleskasasira01@gmail.com>
2021-12-24 12:10:53 +0300

    code refactor

--- src/pages/Claims.js
+++ src/pages/Claims.js
@@ -101,7 +101,7 @@ function Claims() {
     const [clients, setClients] = useState(data);
     const [q, setQ] = useState('');
 
-    const columnHeading = ["Ref Number", "Claimant Details", "Date of Incident", "Number Plate", "Sticker Number", "Claim Estimate", "Status", ""]
+    const columnHeading = ["Ref Number", "Claimant Details", "Date of Incident", "Number Plate", "Sticker Number", "Claim Estimate", "Status", "Action"]
     const columns = ["contact", "name", "createdAt", "contact", "contact", "amount", "status"]
     const search = rows => rows.filter(row =>
         columns.some(column => row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1,));
