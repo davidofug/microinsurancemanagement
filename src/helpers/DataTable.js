bfdeeb629e94612707bddd3d33eb4804fdfc7f3f
Charles Kasasira <charleskasasira01@gmail.com>
2021-12-27 13:14:08 +0300

    Responsive Setting

--- src/helpers/DataTable.js
+++ src/helpers/DataTable.js
@@ -22,7 +22,7 @@ export default function Datatable({ data, columns, columnHeading }) {
 
 export function EditableDatable({ columns, columnHeading, editContactId, handleDeleteClick, handleEditClick, currentClients, editFormData, handleEditFormChange, handleCancelClick }){
   return (
-    <Table bordered hover striped responsivecellPadding={0} cellSpacing={0}>
+    <Table bordered hover striped responsive cellPadding={0} cellSpacing={0}>
       <thead>
           <tr>{columnHeading.map(heading => (<th>{heading}</th>))}</tr>
       </thead>
