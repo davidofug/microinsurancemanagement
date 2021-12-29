2eca1966409578ec501681ef14d7fcef54a523dc
David Wampamba <davidwampamba@gmail.com>
2021-12-29 23:59:37 +0300

    Loader styles

--- src/parts/Loader.js
+++ src/parts/Loader.js
@@ -2,8 +2,12 @@ import React from 'react'
 import '../styles/loader.css'
 function Loader() {
     return (
-        <div className="loader">
-            <h1>Loading</h1>
+        <div className="loader-container">
+            <div className="lds-ripple">
+                <div></div>
+                <div></div>
+            </div>
+            <p className="caption">Please wait!</p>
         </div>
     )
 }
