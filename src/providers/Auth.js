418ef90c3d89cd33dd17f111a35b003bc40116c0
David Wampamba <davidwampamba@gmail.com>
2021-12-29 16:59:13 +0300

    Claims implementation

--- src/providers/Auth.js
+++ src/providers/Auth.js
@@ -5,17 +5,20 @@ function AuthProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
     const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
+    const [authClaims, setAuthClaims] = useState(null)
+/*     useEffect(() => {
         const loggedIn = parseInt(localStorage.getItem('loggedIn'))
         if(loggedIn === 1 || loggedIn === 2 || loggedIn === 3 || loggedIn === 4) {
             setCurrentUser(loggedIn)
         }
-    },[])
+    },[]) */
+
 
     const value = {
         loading,
         currentUser,
+        authClaims,
+        setAuthClaims,
         setCurrentUser,
         setLoading
     }
