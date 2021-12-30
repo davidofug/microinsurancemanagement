fffbf5f94be14d707b0af61ea9c72c0de078e7e4
Charles Kasasira <charleskasasira01@gmail.com>
2021-12-30 19:42:50 +0300

    adding and retrieving from firebase

--- src/pages/Claims.js
+++ src/pages/Claims.js
@@ -5,10 +5,11 @@ import Pagination from '../helpers/Pagination';
 import ClaimTable from '../parts/ClaimTable';
 import SearchBar from '../parts/searchBar/SearchBar';
 import Header from '../parts/header/Header';
-import { Table } from 'react-bootstrap'
+import { Table, Alert } from 'react-bootstrap'
 import { db } from '../helpers/firebase'
 import { collection, getDocs, addDoc, doc} from 'firebase/firestore'
-
+import { FaEllipsisV } from 'react-icons/fa'
+ 
 function Claims() {
   const [claims, setClaims] = useState([])
   const claimsCollectionRef = collection(db, "claims")
@@ -16,10 +17,16 @@ function Claims() {
     useEffect(() => {
       document.title = 'Britam - Claims'
 
+
       const getClaims = async () => {
         const data = await getDocs(claimsCollectionRef)
-        console.log(data)
+        // console.log(data)
+        console.log(data.docs)
+        setClaims(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
       }
+      // setClaims(data)
+      // getClaims()
+      // console.log(data)
       getClaims()
     }, [])
 
@@ -28,65 +35,65 @@ function Claims() {
     
     
   //
-    const [editFormData, setEditFormData] = useState({
-        name: "",
-        gender: "",
-        email: "",
-        contact: "",
-        address: "",
-    });
+  //   const [editFormData, setEditFormData] = useState({
+  //       name: "",
+  //       gender: "",
+  //       email: "",
+  //       contact: "",
+  //       address: "",
+  //   });
 
-  const [editContactId, setEditContactId] = useState(null);
+  // const [editContactId, setEditContactId] = useState(null);
 
-  const handleEditFormChange = (event) => {
-    event.preventDefault();
+  // const handleEditFormChange = (event) => {
+  //   event.preventDefault();
 
-    const fieldName = event.target.getAttribute("name");
-    const fieldValue = event.target.value;
+  //   const fieldName = event.target.getAttribute("name");
+  //   const fieldValue = event.target.value;
 
-    const newFormData = { ...editFormData };
-    newFormData[fieldName] = fieldValue;
+  //   const newFormData = { ...editFormData };
+  //   newFormData[fieldName] = fieldValue;
 
-    setEditFormData(newFormData);
-  };
+  //   setEditFormData(newFormData);
+  // };
 
 
-  const handleEditFormSubmit = (event) => {
-    event.preventDefault();
+  // const handleEditFormSubmit = (event) => {
+  //   event.preventDefault();
 
-    const editedContact = {
-      id: editContactId,
-      name: editFormData.name,
-      gender: editFormData.gender,
-      email: editFormData.email,
-      contact: editFormData.contact,
-      address: editFormData.address,
-    };
+  //   const editedContact = {
+  //     id: editContactId,
+  //     name: editFormData.name,
+  //     gender: editFormData.gender,
+  //     email: editFormData.email,
+  //     contact: editFormData.contact,
+  //     address: editFormData.address,
+  //   };
     
-    const newClaims = [...claims];
+  //   const newClaims = [...claims];
 
-    const index = claims.findIndex(claim => claim.id === editContactId);
+  //   const index = claims.findIndex(claim => claim.id === editContactId);
 
-    newClaims[index] = editedContact;
+  //   newClaims[index] = editedContact;
 
-    setClaims(newClaims);
-    setEditContactId(null);
-  };
+  //   setClaims(newClaims);
+  //   setEditContactId(null);
+  // };
 
-  const handleEditClick = (event, contact) => {
-    event.preventDefault();
-    setEditContactId(contact.id);
+  // const handleEditClick = (event, contact) => {
+  //   event.preventDefault();
+  //   setEditContactId(contact.id);
 
-    const formValues = {
-      name: contact.name,
-      gender: contact.gender,
-      email: contact.email,
-      contact: contact.contact,
-      address: contact.address,
-    };
+  //   const formValues = {
+  //     name: contact.name,
+  //     gender: contact.gender,
+  //     email: contact.email,
+  //     contact: contact.contact,
+  //     address: contact.address,
+  //   };
 
-    setEditFormData(formValues);
-  };
+  //   setEditFormData(formValues);
+  // };
 
   //
 
@@ -101,28 +108,28 @@ function Claims() {
 
 
 
-    const handleDeleteClick = (claimId) => {
-        const newClaims = [...claims];
-        const index = claims.findIndex((claim) => claim.id === claimId);
-        newClaims.splice(index, 1);
-        setClaims(newClaims);
-      };
+    // const handleDeleteClick = (claimId) => {
+    //     const newClaims = [...claims];
+    //     const index = claims.findIndex((claim) => claim.id === claimId);
+    //     newClaims.splice(index, 1);
+    //     setClaims(newClaims);
+    //   };
   
-      const handleCancelClick = () => {
-        setEditContactId(null);
-      };
+    //   const handleCancelClick = () => {
+    //     setEditContactId(null);
+    //   };
 
 
-    const [q, setQ] = useState('');
+    // const [q, setQ] = useState('');
 
     
-    const columns = ["contact", "name", "createdAt", "contact", "contact", "amount", "status"]
-    const search = rows => rows.filter(row =>
-        columns.some(column => row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1,));
+    // const columns = ["contact", "name", "createdAt", "contact", "contact", "amount", "status"]
+    // const search = rows => rows.filter(row =>
+    //     columns.some(column => row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1,));
 
-        const handleSearch = ({target}) => setQ(target.value)
+    //     const handleSearch = ({target}) => setQ(target.value)
 
-
+      // setClaims(search(claims))
     return (
         <div className='components'>
             <Header title="Claims" subtitle="MANAGING CLAIMS" />
@@ -136,31 +143,55 @@ function Claims() {
             </div>
 
             <div className="table-card componentsData">   
-                <div id="search">
-                            <SearchBar placeholder={"Search for claim"} value={q} handleSearch={handleSearch}/>
+                      <div id="search">
+                            {/* <SearchBar placeholder={"Search for claim"} value={q} handleSearch={handleSearch}/> */}
                             <div></div>
                             <div></div>
                       </div>
 
-                      <Table></Table>
-
-                      {/* <form onSubmit={handleEditFormSubmit}>
-                        <ClaimTable 
-                            columns={columns}
-                            editContactId={editContactId}
-                            currentClients={search(currentClaims)}
-                            handleDeleteClick={handleDeleteClick}
-                            handleEditClick={handleEditClick}
-                            editFormData={editFormData}
-                            handleEditFormChange={handleEditFormChange}
-                            handleCancelClick={handleCancelClick}
-                        /> 
-                  </form> */}
+                      <Table hover striped responsive>
+                        <thead>
+                          <tr><th>Ref Number</th><th>Claimant Details</th><th>Date of Incident</th><th>Number Plate</th><th>Sticker Number</th><th>Claim Estimate</th><th>Status</th><th>Action</th></tr>
+                        </thead>
+                        <tbody>
+                          {claims.map((claim, index) => (
+                            <tr key={index}>
+                              <td>{claim.contact}</td>
+                              <td>{claim.name}</td>
+                              <td>{claim.createdAt}</td>
+                              <td>{claim.contact}</td>
+                              <td>{claim.contact}</td>
+                              <td>{claim.amount}</td>
+                              <td><Alert style={{"backgroundColor": "#1475cf", "color": "#fff", "padding": "5px", "textAlign": "center", "border": "none", "margin": "0"}}>{claim.status}</Alert></td>
+                              <td className='started'>
+                        <FaEllipsisV className={`actions please${index}`} onClick={() => {
+                            document.querySelector(`.please${index}`).classList.add('hello')
+                        }} />
+                        <ul id="actionsUl" className='actions-ul'>
+                            <li><button >View Notification</button></li>
+                            <li><button >Claim Settlement</button></li>
+                            <li><button >View Settlement</button></li>
+                            <li><button >Cancel</button></li>
+                            <li><button >Delete</button></li>
+                            <li><button >Edit</button></li>
+                            <hr style={{"color": "black"}}></hr>
+                            <li><button onClick={() => {
+                              document.querySelector(`.please${index}`).classList.remove('hello')
+                            }}>close</button></li>
+                        </ul>
+                    </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                        <tfoot>
+                          <tr><th>Ref Number</th><th>Claimant Details</th><th>Date of Incident</th><th>Number Plate</th><th>Sticker Number</th><th>Status</th><th>Action</th></tr>
+                        </tfoot>
+                      </Table>
 
                   <Pagination 
                     pages={totalPagesNum}
                     setCurrentPage={setCurrentPage}
-                    currentClients={currentClaims}
+                    currentClients={claims}
                     sortedEmployees={data}
                     entries={'claims'} />
 
