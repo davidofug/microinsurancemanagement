b980f34231c121784d0fab8974006981a53d1c5e
Charles Kasasira <charleskasasira01@gmail.com>
2022-01-21 13:28:35 +0300

    different admins different agents

--- src/pages/Agents.js
+++ src/pages/Agents.js
@@ -27,9 +27,14 @@ function Agents() {
           const myAgents = data.filter(user => user.role.agent === true).filter(agent => agent.meta.added_by_uid === authentication.currentUser.uid)
           setAgents(myAgents)
       }).catch()
-    } else{
+    } else if(authClaims.admin){
       listUsers().then(({data}) => {
-        const myAgents = data.filter(user => user.role.agent === true)
+        const mySupervisors = data.filter(user => user.role.supervisor === true).filter(supervisor => supervisor.meta.added_by_uid === authentication.currentUser.uid).map(supervisoruid => supervisoruid.uid)
+
+        const agentsUnderAdmin = [ ...mySupervisors, authentication.currentUser.uid ]
+
+        const myAgents = data.filter(user => user.role.agent === true).filter(agent => agentsUnderAdmin.includes(agent.meta.added_by_uid))
+
         setAgents(myAgents)
     }).catch()
     }
@@ -40,35 +45,32 @@ function Agents() {
   const handleSearch = ({ target: {value} }) => setSearchText(value);
   const searchByName = (data) => data.filter(row => row.name.toLowerCase().indexOf(searchText.toLowerCase()) > -1)
 
+  // Pagination
+  const [ currentPage, setCurrentPage ] = useState(1)
+  const [clientsPerPage] = useState(10)
+  const indexOfLastAgent = currentPage * clientsPerPage
+  const indexOfFirstClient = indexOfLastAgent - clientsPerPage
+  const currentAgents = !agents || searchByName(agents).slice(indexOfFirstClient, indexOfLastAgent)
+  const totalPagesNum = !agents || Math.ceil(agents.length / clientsPerPage)
 
-
-    // Pagination
-    const [ currentPage, setCurrentPage ] = useState(1)
-    const [clientsPerPage] = useState(10)
-    const indexOfLastAgent = currentPage * clientsPerPage
-    const indexOfFirstClient = indexOfLastAgent - clientsPerPage
-    const currentAgents = !agents || searchByName(agents).slice(indexOfFirstClient, indexOfLastAgent)
-    const totalPagesNum = !agents || Math.ceil(agents.length / clientsPerPage)
-
-    // delete agent
-    const handleDelete = async (id) => {
-      const deleteUser = httpsCallable(functions, 'deleteUser')
-      deleteUser({uid:id}).then().catch(err => {
-        console.log(err)
-      })
-      getAgents()
-    };
-
+  // delete agent
+  const handleDelete = async (id) => {
+    const deleteUser = httpsCallable(functions, 'deleteUser')
+    deleteUser({uid:id}).then().catch(err => {
+      console.log(err)
+    })
+    getAgents()
+  };
     
-    // actions context
-    const [showContext, setShowContext] = useState(false)
-    window.onclick = function(event) {
-        if (!event.target.matches('.sharebtn')) {
-            setShowContext(false)
-        }
-    }
+  // actions context
+  const [showContext, setShowContext] = useState(false)
+  window.onclick = function(event) {
+      if (!event.target.matches('.sharebtn')) {
+          setShowContext(false)
+      }
+  }
 
-    const [clickedIndex, setClickedIndex] = useState(null)
+  const [clickedIndex, setClickedIndex] = useState(null)
 
     return (
         <div className='components'>
@@ -100,7 +102,7 @@ function Agents() {
 
                   <Table hover striped responsive>
                         <thead>
-                            <tr><th>#</th><th>Name</th><th>Email</th><th>Gender</th><th>Contact</th><th>Address</th><th>Action</th></tr>
+                            <tr><th>#</th><th>Name</th><th>Email</th><th>Gender</th><th>Contact</th><th>Address</th>{authClaims.admin && <th>Added by</th>}<th>Action</th></tr>
                         </thead>
                         <tbody>
                           {agents.map((agent, index) => (
@@ -111,7 +113,7 @@ function Agents() {
                               <td>{agent.meta.gender}</td>
                               <td>{agent.meta.phone}</td>
                               <td>{agent.meta.address}</td>
-                              
+                              {authClaims.admin && <td>{agent.meta.added_by_name}</td>}
                               <td className="started">
                                 <button className="sharebtn" onClick={() => {setClickedIndex(index); setShowContext(!showContext)}}>&#8942;</button>
 
@@ -153,7 +155,7 @@ function Agents() {
                           ))}
                         </tbody>
                         <tfoot>
-                            <tr><th>#</th><th>Name</th><th>Email</th><th>Gender</th><th>Contact</th><th>Address</th><th>Action</th></tr>
+                            <tr><th>#</th><th>Name</th><th>Email</th><th>Gender</th><th>Contact</th><th>Address</th>{authClaims.admin && <th>Added by</th>}<th>Action</th></tr>
                         </tfoot>
                     </Table>
 
