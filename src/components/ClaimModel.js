abb19c5c1f5fe671724092b2fdcc581baa58ea35
Charles Kasasira <charleskasasira01@gmail.com>
2022-01-25 11:22:39 +0300

    claims pagination

--- src/components/ClaimModel.js
+++ src/components/ClaimModel.js
@@ -1,14 +1,155 @@
 import { Table, Modal, Form, Col, Row, Button } from "react-bootstrap";
 
-function ClaimModel({ singleDoc, handleClose, handleFieldChange }) {
-    const modalSubmit = (event) => {
-        event.preventDefault()
-        console.log("submitted")
-    }
-    
+export function ClaimModelNotification({ singleDoc }) {
   return <div>
       <Modal.Header closeButton>
-          <Modal.Title>Edit Claim</Modal.Title>
+          <Modal.Title>View Claim Settlement</Modal.Title>
+        </Modal.Header>
+        <Form id="update_claim">
+          <Modal.Body>
+                <div className="mb-3">
+                  Status:
+                  <span style={{backgroundColor: "#337ab7", padding: ".4em .6em", borderRadius: ".25em", color: "#fff", fontSize: "85%"}}
+                  > {singleDoc.status}</span>
+                </div>
+                
+
+          <h5>Client Details</h5>
+            <Row className="mb-3">
+              <Form.Group
+                as={Col}
+                style={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "align-items": "start",
+                }}
+              >
+                <Form.Label htmlFor="claimantName">Name</Form.Label>
+                <p>{singleDoc.claimantName}</p>
+              </Form.Group>
+              <Form.Group
+                as={Col}
+                style={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "align-items": "start",
+                }}
+              >
+                <Form.Label htmlFor="claimantEmail">Email Address</Form.Label>
+                <p>{singleDoc.claimantEmail}</p>
+              </Form.Group>
+            </Row>
+
+            <Form.Group
+                as={Col}
+                style={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "align-items": "start",
+                }}
+              >
+                <Form.Label htmlFor="claimantPhoneNumber">
+                  Phone Number
+                </Form.Label>
+                <p>{singleDoc.claimantPhoneNumber}</p>
+              </Form.Group>
+
+              <hr />
+            
+
+
+            <Row className="mb-3">
+              <Form.Group
+                as={Col}
+                style={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "align-items": "start",
+                }}
+              >
+                <Form.Label htmlFor="dateReported">Date Reported</Form.Label>
+                <p>{singleDoc.dateReported}</p>
+              </Form.Group>
+              <Form.Group
+                as={Col}
+                style={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "align-items": "start",
+                }}
+              >
+                <Form.Label htmlFor="policyType">Policy Type</Form.Label>
+                <p>{singleDoc.policyType}</p>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              
+              <Form.Group
+                as={Col}
+                style={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "align-items": "start",
+                }}
+              >
+                <Form.Label htmlFor="stickerNumber">Sticker No.</Form.Label>
+                <p>{singleDoc.stickerNumber}</p>
+              </Form.Group>
+
+              <Form.Group
+                as={Col}
+                style={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "align-items": "start",
+                }}
+              >
+                <Form.Label htmlFor="numberPlate">Plate No.</Form.Label>
+                <p>{singleDoc.numberPlate}</p>
+              </Form.Group>
+            </Row>
+            
+            <Row>
+              
+              <Form.Group
+                as={Col}
+                style={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "align-items": "start",
+                }} className="mb-3"
+              >
+                <Form.Label htmlFor="dateOfIncident">
+                  Date of Incident
+                </Form.Label>
+                <p>{singleDoc.dateOfIncident}</p>
+              </Form.Group>
+            </Row>
+
+            <Row className="mb-3">
+              <Form.Group
+                as={Col}
+                style={{
+                  display: "flex",
+                  "flex-direction": "column",
+                  "align-items": "start",
+                }}
+              >
+                <Form.Label htmlFor="estimate">Claim Estimate</Form.Label>
+                <p>{singleDoc.estimate}</p>
+              </Form.Group>
+            </Row>
+            
+          </Modal.Body>
+        </Form>
+  </div>;
+}
+
+export function ClaimModel({singleDoc, handleClose, handleFieldChange, modalSubmit}) {
+
+    return <div>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit {singleDoc.claimantName}'s Claim</Modal.Title>
         </Modal.Header>
         <Form id="update_claim" onSubmit={modalSubmit}>
           <Modal.Body>
@@ -190,20 +331,18 @@ function ClaimModel({ singleDoc, handleClose, handleFieldChange }) {
                 />
               </Form.Group>
             </Row>
-            <Button
+            
+          </Modal.Body>
+          <Modal.Footer>
+          <Button
               variant="primary"
               type="submit"
-              onClick={() => {
-                handleClose();
-              }}
+              onClick={handleClose}
               id="submit"
             >
               Submit
             </Button>
-          </Modal.Body>
-          <Modal.Footer></Modal.Footer>
+          </Modal.Footer>
         </Form>
-  </div>;
+    </div>
 }
-
-export default ClaimModel;
