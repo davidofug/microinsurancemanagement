0d7446f137ad859dbc9e862c3f0e42c3a3118f73
Charles Kasasira <charleskasasira01@gmail.com>
2022-01-29 10:47:02 +0300

    send reset password email

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import useAuth from "../contexts/Auth";
 import logo from "../assets/imgs/britam-logo.png";
 import { Link, Redirect } from "react-router-dom";
+import { sendPasswordResetEmail, getAuth } from 'firebase/auth'
 
 import "../assets/styles/login.css";
 
@@ -10,23 +11,36 @@ function ForgotPassword() {
 
   let { setCurrentUser } = useAuth();
 
+  const auth = getAuth()
+
   useEffect(() => {
     const loggedIn = Number(localStorage.getItem("loggedIn"));
-
     if (loggedIn === 1 || loggedIn === 2 || loggedIn === 3 || loggedIn === 4) {
       setCurrentUser(loggedIn);
       setLogin(loggedIn);
     }
-
     document.title = "Britam - With you every step of the way";
   });
+
+  const handleResetPassword = (event) => {
+    event.preventDefault()
+    console.log("started")
+    sendPasswordResetEmail(auth, event.target.email.value)
+      .then(() => {
+        console.log('Password reset email sent')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+      console.log("Ended")
+  }
 
   if (isLogin) return <Redirect to={{ pathname: "/supervisor-dashboard" }} />;
 
   return (
     <div className="auth-wrapper">
       <img src={logo} alt="Britam" />
-      <form action="">
+      <form onSubmit={handleResetPassword}>
         <p>Forgot Password?</p>
         <p id="enter">Enter your e-mail address to reset your password</p>
         <div className="login-inputs">
