2983de505144f4e58be19b4185a7cfc96e54fc76
Charles Kasasira <charleskasasira01@gmail.com>
2022-02-02 17:29:38 +0300

    SupervisorDetails

--- src/components/SupervisorDetails.js
+++ src/components/SupervisorDetails.js
@@ -15,6 +15,7 @@ function SupervisorDetails({ name, user_id }) {
     }, [])
 
     const [policies, setPolicies] = useState([])
+    const [ agents, setAgents ] = useState([])
     const policyCollectionRef = collection(db, "policies");
 
     const getPolicies = async() => {
@@ -24,28 +25,30 @@ function SupervisorDetails({ name, user_id }) {
         const listUsers = httpsCallable(functions, 'listUsers')
         
         listUsers().then(({data}) => {
-            const myAgents = data.filter(user => user.role.agent).filter(agent => agent.meta.added_by_uid === user_id).map(agentuid => agentuid.uid)
-            
-            const usersUnderSupervisor = [ ...myAgents, user_id ]
+            const myAgents = data.filter(user => user.role.agent).filter(agent => agent.meta.added_by_uid === user_id)
+            const myAgentsID = myAgents.map(agentuid => agentuid.uid)
+            const usersUnderSupervisor = [ ...myAgentsID, user_id ]
 
+            myAgents.length === 0 ? setAgents(null) : setAgents(myAgents)
             const supervisorMtpPolicies = policiesArray.filter(policy => usersUnderSupervisor.includes(policy.added_by_uid))
             supervisorMtpPolicies.length === 0 ? setPolicies(null) : setPolicies(supervisorMtpPolicies)
         })
     }
-
+console.log(agents)
 
     return (
         <>
             <Modal.Header closeButton>
-                <Modal.Title>{name}'s Issued stickers</Modal.Title>
+                <Modal.Title>{name}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
+                <h6>Stickers Sold Under {name}</h6>
                 {policies !== null && policies.length > 0
                 ?
                     <Table responsive hover striped>
                         <thead>
                             <tr>
-                                <th>Client</th><th>StickerNo.</th><th>Start Date</th><th>End Date</th><th>No.Plate</th><th>Commission</th><th>Status</th><th>Added by</th>
+                                <th>Client</th><th>StickerNo.</th><th>Start Date</th><th>End Date</th><th>No.Plate</th><th>Total Valuation</th><th>Commission</th><th>Status</th><th>Added by</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -56,6 +59,7 @@ function SupervisorDetails({ name, user_id }) {
                                     <td>{policy.clientDetails && policy.policyStartDate}</td>
                                     <td>{policy.clientDetails && policy.policyEndDate}</td>
                                     <td>{policy.stickersDetails && policy.stickersDetails[0].plateNo}</td>
+                                    <td>{policy.totalValuation}</td>
                                     <td>2,191</td>
                                     <td>
                                         {policy.stickersDetails && policy.stickersDetails[0].status === 'new'  && 
@@ -88,6 +92,11 @@ function SupervisorDetails({ name, user_id }) {
                                 </tr>
                             ))}
                         </tbody>
+                        <tfoot>
+                            <tr>
+                                <th colSpan={2}>Grand Totals</th><th></th><th></th><th></th><th></th><th></th><th></th><th></th>
+                            </tr>
+                        </tfoot>
                     </Table>
                 :
                     policies === null
@@ -100,6 +109,50 @@ function SupervisorDetails({ name, user_id }) {
                     :
                     <Loader />
                 }
+
+                <div className='mt-3'>
+                    <h6>Agents under {name}</h6>
+
+                    {agents !== null && agents.length > 0
+                ?
+                    <Table responsive hover striped>
+                        <thead>
+                            <tr>
+                                <th>Name</th><th>Email</th><th>Category</th><th>Gender</th><th>Contact</th><th>Address</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {agents.map(agent => (
+                                <tr>
+                                    <td>{agent.name}</td>
+                                    <td>{agent.email}</td>
+                                    <td>
+                                        {agent.role.mtp && <div>MTP</div>}
+                                        {agent.role.comprehensive && <div>Comprehensive</div>}
+                                        {agent.role.windscreen && <div>Windscreen</div>}
+                                        {agent.role.newImport && <div>New Import</div>}
+                                        {agent.role.transit && <div>Transit</div>}
+                                    </td>
+                                    <td>{agent.meta.gender}</td>
+                                    <td>{agent.meta.phone}</td>
+                                    <td>{agent.meta.address}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                :
+                    policies === null
+                    ?
+                    <div className="no-table-data">
+                        <i><ImFilesEmpty /></i>
+                        <h4>No agents yet</h4>
+                        <p>{name} has no agents yet</p>
+                    </div>
+                    :
+                    <Loader />
+                }
+                </div>
+                
             </Modal.Body>
         </>
     )
