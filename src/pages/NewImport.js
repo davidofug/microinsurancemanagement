1c6ae47d1845f47e57a0a17af581ea9babfe5845
Charles Kasasira <charleskasasira01@gmail.com>
2022-02-04 16:48:08 +0300

    sorting sorting stickers

--- src/pages/NewImport.js
+++ src/pages/NewImport.js
@@ -36,7 +36,7 @@ export default function Mtp() {
   const getMTP = async () => {
     const data = await getDocs(policyCollectionRef);
     const policiesArray = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-    const mtpPolicies = policiesArray.filter(policy => policy.category === 'newImport')
+    const mtpPolicies = policiesArray.filter(policy => policy.category === 'newImport').sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
     
     // agent mtp policies
     if(authClaims.agent){
--- src/pages/Transit.js
+++ src/pages/Transit.js
@@ -36,7 +36,7 @@ export default function Mtp() {
   const getMTP = async () => {
     const data = await getDocs(policyCollectionRef);
     const policiesArray = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-    const mtpPolicies = policiesArray.filter(policy => policy.category === 'transit')
+    const mtpPolicies = policiesArray.filter(policy => policy.category === 'transit').sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
     
     // agent mtp policies
     if(authClaims.agent){
