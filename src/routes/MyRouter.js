9c4b577b4f251f062c3d4232fabc8fdbc339012d
Charles Kasasira <charleskasasira01@gmail.com>
2022-03-10 12:41:55 +0300

    created an index for routes

--- src/routes/MyRouter.js
+++ src/routes/MyRouter.js
@@ -4,19 +4,15 @@ import Logout from '../pages/Logout'
 import useAuth from '../contexts/Auth'
 import ForgotPassword from '../pages/ForgotPassword'
 
-import AdminRoutes from './AdminRoutes'
-import AgentsRoutes from './AgentsRoutes'
-import SupervisorRoutes from './SupervisorRoutes'
-import SuperAdminRoutes from './SuperAdminRoutes'
+import { SuperAdminRoutes, AdminRoutes, SupervisorRoutes, AgentsRoutes } from '.'
 
 import AdminMenu from '../pages/admin/AdminMenu'
-import SupervisorMenu from '../pages/supervisor/SupervisorMenu'
-
 import AgentMtpMenu from '../pages/agent/AgentMtpMenu'
 import AgentCompMenu from '../pages/agent/AgentCompMenu'
 import AgentMtpCompMenu from '../pages/agent/AgentMtpCompMenu'
-import AgentMtpCompWindMenu from '../pages/agent/AgentMtpCompWindMenu'
 import SuperAdminMenu from '../pages/superAdmin/SuperAdminMenu'
+import SupervisorMenu from '../pages/supervisor/SupervisorMenu'
+import AgentMtpCompWindMenu from '../pages/agent/AgentMtpCompWindMenu'
 
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import SuperAdminRoutes from './SuperAdminRoutes'
+import AdminRoutes from './AdminRoutes'
+import SupervisorRoutes from './SupervisorRoutes'
+import AgentsRoutes from './AgentsRoutes'
+
+
+export {
+    SuperAdminRoutes,
+    AdminRoutes,
+    SupervisorRoutes,
+    AgentsRoutes
+}
