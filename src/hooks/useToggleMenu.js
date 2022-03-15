bf976909e5c5cfa7ccb991ddc3c7456b0b352226
Charles Kasasira <charleskasasira01@gmail.com>
2022-03-15 11:08:43 +0300

    menu refactor

--- src/hooks/useToggleMenu.js
+++ src/hooks/useToggleMenu.js
@@ -3,8 +3,16 @@ import { useState } from "react";
 const useToggleMenu = () => {
     const [ largeContentClass, setLargeContentClass ] = useState(localStorage.getItem('preferredToggleMenu') ? !JSON.parse(localStorage.getItem('preferredToggleMenu')) : false)
 
-    const minimiseMenu = () => setLargeContentClass(true)
-    const maximiseMenu = () => setLargeContentClass(false)
+    const minimiseMenu = () => {
+        setLargeContentClass(true)
+        localStorage.setItem('preferredToggleMenu', false)
+    }
+    const maximiseMenu = () => {
+        setLargeContentClass(false)
+        localStorage.setItem('preferredToggleMenu', true)
+    }
 
     return [largeContentClass, minimiseMenu, maximiseMenu]
 }
+
+export default useToggleMenu
