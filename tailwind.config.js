971c571c56d7c98826fa4cf130520103ce9335ae
Charles Kasasira <charleskasasira01@gmail.com>
2023-01-09 10:49:34 +0300

    fix: system logs filter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: "tw-",
+  important: true,
+  corePlugins: {
+    preflight: false,
+  },
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
