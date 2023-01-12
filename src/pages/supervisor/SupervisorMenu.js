10680675c3815dbac290cb302b590e3d88e9b6b1
Charles Kasasira <charleskasasira01@gmail.com>
2023-01-12 19:33:38 +0300

    update onrefresh key

--- src/pages/supervisor/SupervisorMenu.js
+++ src/pages/supervisor/SupervisorMenu.js
@@ -122,7 +122,7 @@ function SupervisorMenu({ minimiseMenu, maximiseMenu }) {
                 <MdSettings /> Account Settings
               </div>
 
-              <hr class="tw-my-3 tw-border-gray-600" />
+              <hr className="tw-my-3 tw-border-gray-600" />
 
               <li
                 onClick={handleLogout}
@@ -181,7 +181,7 @@ function SupervisorMenu({ minimiseMenu, maximiseMenu }) {
                 <MdSettings /> Account Settings
               </div>
 
-              <hr class="tw-my-3 tw-border-gray-600" />
+              <hr className="tw-my-3 tw-border-gray-600" />
 
               <li
                 onClick={handleLogout}
