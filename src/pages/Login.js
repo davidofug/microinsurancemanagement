0164f16dbb9e6579c2306666928f56bb0deb9cd3
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-01-18 17:49:48 +0300

    Updating logo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import useAuth from "../contexts/Auth";
-import { Navigate, Link, useNavigate, redirect } from "react-router-dom";
-import logo from "../assets/imgs/SWICO-LOGO.png";
+import { Navigate, Link } from "react-router-dom";
 import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 import { authentication, onAuthStateChange } from "../helpers/firebase";
-import { signInWithEmailAndPassword, setPersistence } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { Alert } from "react-bootstrap";
 import "../assets/styles/login.css";
 import Loader from "../components/Loader";
@@ -13,12 +12,10 @@ function Login() {
   const [user, setUser] = useState({ email: "", password: "" });
   const [password, setPassword] = useState("password");
   const [isVisible, setIsVisible] = useState(false);
-
-  const navigate = useNavigate();
-
-  const { currentUser, setCurrentUser, authClaims, setAuthClaims } = useAuth();
+  const { currentUser, setCurrentUser, authClaims, setAuthClaims, logo } = useAuth();
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
+
 
   const pageOnRefreshSuperAdmin =
     localStorage.getItem("onRefresh") || "/superadmin/dashboard";
