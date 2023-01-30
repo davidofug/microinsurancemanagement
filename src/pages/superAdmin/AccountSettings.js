50f56a29108c64273c8b95eb381037634754f775
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-01-30 15:16:19 +0300

    Account settings

--- src/pages/superAdmin/AccountSettings.js
+++ src/pages/superAdmin/AccountSettings.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "../../styles/Settings.css";
 import Header from "../../components/header/Header";
 import useDialog from "../../hooks/useDialog";
